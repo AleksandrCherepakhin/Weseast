310fad765b4fe1f916837706372fea62c4da2015
Oleksandr <4repakhin@gmail.com>
2020-10-30 01:09:02 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var prevScrollpos = window.pageYOffset;
+let header = document.querySelector(".header");
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    header.classList.remove('hide');
+  } else {
+    header.classList.add('hide');
+  }
+  prevScrollpos = currentScrollPos;
+};
